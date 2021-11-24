db5286aaa0b090c1ac651f2cc2a3fa53ad0ac5d8
Ashwin Vijendra <ashwinvijendra@gmail.com>
2021-11-24 19:55:37 +0530

    added jsx JS

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const title = "Love you to the moon and back";
+const author = "Tim Warness";
+const img =
+  "https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY436_QL65_.jpg";
+
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
@@ -16,26 +21,8 @@ function BookList() {
   return (
     <section className="booklist">
       <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
-
-const Image = () => (
-  <img
-    src="https://m.media-amazon.com/images/I/81eB+7+CkUL._AC_UY436_QL65_.jpg"
-    alt=""
-  />
-);
-
-const Title = () => {
-  return <h1>Love you to the moon and back</h1>;
-};
-
-const Author = () => <h4>Tim Warness</h4>;
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
