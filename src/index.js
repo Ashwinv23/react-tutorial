e8ba3346ba75218973b150f7bd1725bb02e0c707
Ashwin Vijendra <ashwinvijendra@gmail.com>
2021-11-24 17:15:06 +0530

    simple code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Person = () => <h2>Ashwin Vijendra</h2>;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Message = () => {
+  return <p>This is a component</p>;
+};
+
+function Greeting() {
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
+}
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
